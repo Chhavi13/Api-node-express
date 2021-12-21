fcbcb8408732abccfe65f9b1a1667f8f94ebf824
saurabh thoughtwin <saurabh.thoughtwin@gmail.com>
2021-12-21 19:09:20 +0530

    file updated

--- index.js
+++ index.js
@@ -1,37 +1,108 @@
 var express = require('express');
 var app = express();
-const {arr}= require('./userData');
-var bodyParser =require('body-parser');
-app.use(express.urlencoded({ extended: true}));
+
+const { arr } = require('./userData');
+var bodyParser = require('body-parser');
+
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
 
 
 app.get('/', function (req, res) {
-  return res.json({message:"API is working successfully"});    
+  return res.json({ message: "API is working successfully" });
 });
 
 app.get('/api/userData', function (req, res) {
-  return res.json(arr);    
+  return res.json(arr);
 });
 
 app.post('/api/userData', function (req, res) {
-   console.log(req.body)
-   const user={
-      id:arr.length +1,
-      fname:req.body.fname,
-      lname:req.body.lname,
-      city:req.body.city
-    }
-   arr.push(user);
-   console.log(arr);
-   return res.json(arr);    
+  console.log(req.body)
+  const { emailid, lname, fname, city } = req.body;
+  const user = {
+    id: arr.length + 1,
+    fname: req.body.fname,
+    lname: req.body.lname,
+    city: req.body.city,
+    emailid: req.body.emailid
+  }
+  console.log(arr)
+  console.log(fname);
+
+  const findRecord = arr.find(function (element) {
+    return element.emailid === emailid;
+  });
+  if (findRecord) {
+    var errorMessage = 'A user with that email has already registered. Please use a different email..';
+    const err = { status: 400, error: errorMessage };
+    return res.json(err);
+  } else {
+    arr.push(user);
+    return res.json(arr);
+  }
+});
+
+app.put('/userData/:id', function (req, res) {
+  let id = req.params.id;
+  fname = req.body.fname,
+    lname = req.body.lname,
+    city = req.body.city,
+    emailid = req.body.emailid
+  let index = arr.findIndex((data) => {
+    return (data.id == id)
+    
+  });
+   console.log(index)
+  if (index >= 0) {
+    let addData = arr[index]
+    addData.fname = fname;
+    addData.lname = lname;
+    addData.city = city;
+    addData.emailid = emailid;
+    res.json(addData);
+    
+  }
+
+  else {
+    res.status(400);
+  }
+  
+});
+
+app.delete('/userData/:id', function (req, res) {
+  let found = arr.find(function (item) {
+    return item.id === parseInt(req.params.id);
+  });
+  if (found) {
+
+    let targetIndex = arr.indexOf(found);
+
+    // splice means delete item from `data` array using index
+    arr.splice(targetIndex, 1);
+  }
+
+  console.log('item deleted')
+  // return with status 204
+  // success status response code 204 indicates
+  // that the request has succeeded
+  res.json(arr)
 
 });
 
-
-app.listen(8086, function () {
-  console.log('Example app listening on port 8086!');
-  
-  
+app.listen(8085, function () {
+  console.log('Example app listening on port 8085!');
 });
+
+
+
+
+
+// let user = await User.findOne({ email: req.body.email });
+//     if (user)
+//       return res.status(400).send("User with given email already exist!");
+
+//     user = await new User({
+//       name: req.body.name,
+//       email: req.body.email,
+//     }).save();
--- userData.js
+++ userData.js
@@ -3,21 +3,25 @@ const arr = [
         "id": 1,
         "fname": "Ram",
         "lname": "sharma",
-        "city": "indore"
+        "city": "indore",
+        "emailid":"ram1@gmail.com",
+
     },
     {
         "id": 2,
         "fname": "shayam",
         "lname": "sharma",
-        "city": "bhopal"
+        "city": "bhopal",
+        "emailid":"shyam10@gmail.com",
+
     },
     {
         "id": 3,
         "fname": "aditya",
         "lname": "verma",
-        "city": "pune"
+        "city": "pune",
+        "emailid":"aditya11@gmail.com",
     }
-
 ];
 
 console.log(arr[0].id)
